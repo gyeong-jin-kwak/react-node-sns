80a3b6832f743e980e9d37c1fb7e7e9036c09bc8
곽경진 <kgj1212_@naver.com>
2021-03-16 17:53:26 +0900

    [feat] add global style as slick-slide

--- front/.next/static/chunks/webpack.js
+++ front/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "19dc2718d0b257554f66";
+/******/ 	var hotCurrentHash = "88e1ed97ae4f527fa0f6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
